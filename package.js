8f648b1773f1d68b98483368c495dddbdb32a360
Guillaume Dufour <dufour@magillem.com>
2016-10-20 10:24:50 +0200

    fix dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "Dufgui:pace-ng-file-upload-patch",
-  "version": "0.0.1",
+  "version": "0.0.2",
   summary: "patch for PACE and ng-file-upload, see https://github.com/danialfarid/ng-file-upload/issues/832",
   git: "https://github.com/Dufgui/pace-ng-file-upload-patch.git"
 });
